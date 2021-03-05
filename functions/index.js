3871529c4306e96be1de115d49511395c06c0feb
shelleyhowarth <37180741+shelleyhowarth@users.noreply.github.com>
2021-03-05 12:23:52 +0000

    complete

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ const admin = require("firebase-admin");
 
 admin.initializeApp();
 
-exports.sendEmail = functions.firestore
+exports.sendReturn = functions.firestore
     .document("returns/{orderId}")
     .onCreate((snap, context) => {
       admin
@@ -21,3 +21,37 @@ exports.sendEmail = functions.firestore
           })
           .then(() => console.log("Queued email for delivery!"));
     });
+
+exports.sendContact = functions.firestore
+    .document("contactForms/{orderId}")
+    .onCreate((snap, context) => {
+      admin
+          .firestore()
+          .collection("mail")
+          .add({
+            to: snap.data().email,
+            message: {
+              subject: "Golden Shoes: User Query",
+              text: `Name: ${snap.data().firstName} ${snap.data().lastName}
+
+                     Email: ${snap.data().email}
+
+                     Reason: ${snap.data().reason}
+
+                     Order Reference: ${snap.data().orderRef}
+
+                     Description: ${snap.data().description}`,
+              html: `Name: ${snap.data().firstName} ${snap.data().lastName}
+
+                      Email: ${snap.data().email}
+
+                      Reason: ${snap.data().reason}
+
+                      Order Reference: ${snap.data().orderRef}
+                      
+                      Description: ${snap.data().description}`,
+            },
+          })
+          .then(() => console.log("Queued email for delivery!"));
+    });
+
